987d7ae2353e27a60e8224d1718673574abb9b00
HEMANT NAGAR <hemantnagar9520@gmail.com>
2025-06-16 01:30:59 +0530

    Add files via upload

---
+++ netlify/functions/weather.js
@@ -0,0 +1,30 @@
+const fetch = require('node-fetch');
+
+exports.handler = async function(event, context) {
+  const city = event.queryStringParameters.city;
+  const API_KEY = process.env.WEATHER_API_KEY;
+
+  if (!city) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({ error: 'City is required' }),
+    };
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Something went wrong' }),
+    };
+  }
+};
